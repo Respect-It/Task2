24f076d3f2524cafd073870e28d63c134f9aa9c9
Respect It <respectit456@gmail.com>
2025-06-03 16:08:03 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+  
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {useState} from 'react';
+
+export default function Navbar() {
+  const [showmenu, setShowMenu]= useState(false)
+  const handletoggle= () => (
+    setShowMenu(!showmenu)
+  )
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" onClick={handletoggle} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      {
+      showmenu? <i class="bi bi-x-circle"></i> : <span class="navbar-toggler-icon"></span>
+}
+    </button>
+    <div class={`collapse navbar-collapse ${showmenu ? 'show' : ' '}`}  id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  );
+}
