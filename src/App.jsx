62300ff3d67deed7e4a4e9de6c111e81e696f3ab
Respect It <respectit456@gmail.com>
2025-06-05 15:44:00 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ export default function App() {
      </div> 
      </div>
      </div>
-    
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ export default function Navbar() {
           <div className="navbar-item">
             <div className="buttons">
               <a className="button is-primary"><strong>Sign up</strong></a>
-              <a className="button is-light">Log in</a>
+              <a className="button is-light" >Log in</a> 
             </div>
           </div>
         </div>
