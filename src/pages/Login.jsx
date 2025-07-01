786adafda73b77fd98e8a4f5ca7de456946f590d
Respect It <respectit456@gmail.com>
2025-07-01 14:19:15 +0500

    ok

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,36 @@
-import React from 'react'
-
+import React from "react";
 function Login() {
   return (
-    <>
-    <body className="body-login">
-  <div className="wrapper">
-    <form action="">
-      <h1>Login</h1>
-      <div className="input-box">
-        <input type="text" placeholder="Username" required/>
-        <i className='bx bxs-user'></i>
+    <div className="body-login">
+      <div className="wrapper">
+        <form action="">
+          <h1>Login</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+            <i className="bx bxs-user"></i>
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" required />
+            <i className="bx bxs-lock-alt"></i>
+          </div>
+          <div className="remember-forgot">
+            <label>
+              <input type="checkbox" />Remember Me
+            </label>
+            <a href="#">Forgot Password</a>
+          </div>
+          <button type="submit" className="btn">
+            Login
+          </button>
+          <div className="register-link">
+            <p>
+              Don’t Have An Account! <a href="#">Register</a>
+            </p>
+          </div>
+        </form>
       </div>
-      <div className="input-box">
-        <input type="password" placeholder="Password" required/>
-        <i className='bx bxs-lock-alt' ></i>
-      </div>
-      <div className="remember-forgot">
-        <label><input type="checkbox"/>Remember Me</label>
-        <a href="#">Forgot Password</a>
-      </div>
-      <button type="submit" className="btn">Login</button>
-      <div className="register-link">
-        <p>Dont Have An Account ! <a href="#">Register</a></p>
-      </div>
-    </form>
-  </div>
-  </body>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Login
+export default Login;
