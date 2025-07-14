cc6afc2dfee358ee2b4e8118c5ae9ec0eddb2b89
Respect It <respectit456@gmail.com>
2025-07-14 17:09:03 +0500

    model

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import banner from '../assets/banner.webm';
-import '../styles/Banner.css';
+import React from "react";
+import banner from "../assets/banner.webm";
+import "../styles/Banner.css";
 
 const Banner = () => {
   return (
     <section className="banner">
+      <iframe
+        src="https://my.spline.design/genkubgreetingrobot-bDgLe5b5me5mzFDpDsb5KhgG/"
+        frameborder="0"
+        width="100%"
+        height="100%"
+      ></iframe>
       <video
         className="banner-image"
         src={banner}
